924a1f59dec74ac26f7ba07fb0ac1ec6eabc7ea8
Josh Mann <jlmann72@gmail.com>
2020-08-29 10:26:12 -0400

    Migration Practice

---
+++ Migrations/20200829132321_InitialMigration.cs
@@ -0,0 +1,119 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
+
+namespace SuncoastMovies.Migrations
+{
+    public partial class InitialMigration : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Actors",
+                columns: table => new
+                {
+                    Id = table.Column<int>(nullable: false)
+                        .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.IdentityByDefaultColumn),
+                    FullName = table.Column<string>(nullable: true),
+                    Birthday = table.Column<DateTime>(nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Actors", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Ratings",
+                columns: table => new
+                {
+                    Id = table.Column<int>(nullable: false)
+                        .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.IdentityByDefaultColumn),
+                    Description = table.Column<string>(nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Ratings", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Movies",
+                columns: table => new
+                {
+                    Id = table.Column<int>(nullable: false)
+                        .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.IdentityByDefaultColumn),
+                    Title = table.Column<string>(nullable: true),
+                    PrimaryDirector = table.Column<string>(nullable: true),
+                    YearReleased = table.Column<int>(nullable: false),
+                    Genre = table.Column<string>(nullable: true),
+                    RatingId = table.Column<int>(nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Movies", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Movies_Ratings_RatingId",
+                        column: x => x.RatingId,
+                        principalTable: "Ratings",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Roles",
+                columns: table => new
+                {
+                    Id = table.Column<int>(nullable: false)
+                        .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.IdentityByDefaultColumn),
+                    CharacterName = table.Column<string>(nullable: true),
+                    MovieId = table.Column<int>(nullable: false),
+                    ActorId = table.Column<int>(nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Roles", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Roles_Actors_ActorId",
+                        column: x => x.ActorId,
+                        principalTable: "Actors",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_Roles_Movies_MovieId",
+                        column: x => x.MovieId,
+                        principalTable: "Movies",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Movies_RatingId",
+                table: "Movies",
+                column: "RatingId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Roles_ActorId",
+                table: "Roles",
+                column: "ActorId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Roles_MovieId",
+                table: "Roles",
+                column: "MovieId");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Roles");
+
+            migrationBuilder.DropTable(
+                name: "Actors");
+
+            migrationBuilder.DropTable(
+                name: "Movies");
+
+            migrationBuilder.DropTable(
+                name: "Ratings");
+        }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -1,8 +1,8 @@
 ﻿using System;
-using Microsoft.EntityFrameworkCore;
-using System.Linq;
-using Microsoft.Extensions.Logging;
 using System.Collections.Generic;
+using System.Linq;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Logging;
 
 namespace SuncoastMovies
 {
@@ -13,49 +13,35 @@ namespace SuncoastMovies
         public string PrimaryDirector { get; set; }
         public int YearReleased { get; set; }
         public string Genre { get; set; }
+
+        // This is the column in the database
         public int RatingId { get; set; }
+        // This is the related object we can use from our code (if properly used with Include)
         public Rating Rating { get; set; }
+
+        // This is the related list of roles we an use (if properly used with Include)
         public List<Role> Roles { get; set; }
-    }
-    // Define a database context for our Suncoast Movies database.
-    // It derives from (has a parent of) DbContext so we get all the
-    // abilities of a database context from EF Core.
-    class SuncoastMoviesContext : DbContext
-    {
-        // Define a movies property that is a DbSet
-        public DbSet<Movie> Movies { get; set; }
-
-        public DbSet<Rating> Ratings { get; set; }
-        public DbSet<Role> Roles { get; set; }
-        public DbSet<Actor> Actors { get; set; }
-
-        // Define a method required by EF that will configure our connection to the database.
-        // 
-        // DbContextOptionsBuilder is provided to us. We then tell that object 
-        //  we want to connect to a PostgreSQL database named SuncoastMovies on our local machine
-        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
-        {
-            optionsBuilder.UseNpgsql("server=localhost;database=SuncoastMovies");
-
-            var loggerFactory = LoggerFactory.Create(builder => builder.AddConsole());
-            optionsBuilder.UseLoggerFactory(loggerFactory);
-        }
     }
 
     class Rating
     {
         public int Id { get; set; }
         public string Description { get; set; }
-
     }
 
     class Role
     {
         public int Id { get; set; }
         public string CharacterName { get; set; }
+
+        // This is the column in the database
         public int MovieId { get; set; }
+        // This is the related object we can use from our code (if properly used with Include)
         public Movie Movie { get; set; }
+
+        // This is the column in the database
         public int ActorId { get; set; }
+        // This is the related object we can use from our code (if properly used with Include)
         public Actor Actor { get; set; }
     }
 
@@ -64,44 +50,152 @@ namespace SuncoastMovies
         public int Id { get; set; }
         public string FullName { get; set; }
         public DateTime Birthday { get; set; }
+
+        // This is the related list of roles we an use (if properly used with Include)
         public List<Role> Roles { get; set; }
+    }
+
+    // Define a database context for our Suncoast Movies database.
+    // It derives from (has a parent of) DbContext so we get all the
+    // abilities of a database context from EF Core.
+    class SuncoastMoviesContext : DbContext
+    {
+        // Define a movies property that is a DbSet.
+        public DbSet<Movie> Movies { get; set; }
+
+        // Define a Ratings property that is a DbSet.
+        public DbSet<Rating> Ratings { get; set; }
+
+        // Define a Roles property that is a DbSet.
+        public DbSet<Role> Roles { get; set; }
+
+        // Define an Actors property that is a DbSet.
+        public DbSet<Actor> Actors { get; set; }
+
+        // Define a method required by EF that will configure our connection
+        // to the database.
+        //
+        // DbContextOptionsBuilder is provided to us. We then tell that object
+        // we want to connect to a postgres database named suncoast_movies on
+        // our local machine.
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            var loggerFactory = LoggerFactory.Create(builder => builder.AddConsole());
+            optionsBuilder.UseLoggerFactory(loggerFactory);
+
+            optionsBuilder.UseNpgsql("server=localhost;database=SuncoastMovies");
+        }
     }
 
     class Program
     {
         static void Main(string[] args)
         {
-            // Get a new context which will connect to the database
             var context = new SuncoastMoviesContext();
 
-            // Get a reference to our collection of movies/
-            // NOTE: This doesn't yet access any of them, just
-            // gives us a variable that knows how
-            var movies = context.Movies.Include(movie => movie.Rating).Include(movie => movie.Roles).ThenInclude(role => role.Actor);
+            var keepGoing = true;
 
-            // Count the movies
-            var movieCount = movies.Count();
-            Console.WriteLine($"There are {movieCount} movies");
-
-            // List the movies
-            foreach (var movie in movies)
+            while (keepGoing)
             {
-                if (movie.Rating == null)
-                {
-                    Console.WriteLine($"There is a movie named {movie.Title} and has not been rated yet");
-                }
-                else
-                {
-                    Console.WriteLine($"There is a movie named {movie.Title} and a rating of {movie.Rating.Description}");
-                }
+                Console.Write("(L)ist movies. (C)reate movie. (D)elete movie. (U)pdate movie. (Q)uit: ");
+                var option = Console.ReadLine().ToUpper();
 
-                foreach (var role in movie.Roles)
+                switch (option)
                 {
-                    Console.WriteLine($" - Has a character named {role.CharacterName} played by {role.Actor.FullName}");
+                    case "Q":
+                        keepGoing = false;
+                        break;
+
+                    case "L":
+                        var movies = context.Movies.Include(movie => movie.Rating).Include(movie => movie.Roles).ThenInclude(role => role.Actor);
+
+                        var movieCount = movies.Count();
+                        Console.WriteLine($"There are {movieCount} movies!");
+
+                        foreach (var movie in movies)
+                        {
+                            if (movie.Rating == null)
+                            {
+                                Console.WriteLine($"There is a movie named {movie.Title} and has not been rated yet");
+                            }
+                            else
+                            {
+                                Console.WriteLine($"There is a movie named {movie.Title} and a rating of {movie.Rating.Description}");
+                            }
+
+                            foreach (var role in movie.Roles)
+                            {
+                                Console.WriteLine($" - Has a character named {role.CharacterName} played by {role.Actor.FullName}");
+                            }
+                        }
+                        break;
+
+                    case "C":
+                        Console.Write("What is the name of the new movie: ");
+                        var title = Console.ReadLine();
+
+                        Console.Write("What is the name of the primary director: ");
+                        var primaryDirector = Console.ReadLine();
+
+                        Console.Write("What is the genre: ");
+                        var genre = Console.ReadLine();
+
+                        Console.Write("In what year was the movie released: ");
+                        var yearReleased = int.Parse(Console.ReadLine());
+
+                        Console.Write("What is the ID of the movie rating: ");
+                        var ratingID = int.Parse(Console.ReadLine());
+
+                        var newMovie = new Movie
+                        {
+                            Title = title,
+                            PrimaryDirector = primaryDirector,
+                            Genre = genre,
+                            YearReleased = yearReleased,
+                            RatingId = ratingID
+                        };
+
+                        context.Movies.Add(newMovie);
+                        context.SaveChanges();
+                        break;
+
+                    case "U":
+                        Console.Write("What is the name of the movie you want to update: ");
+                        var titleOfMovieToUpdate = Console.ReadLine();
+
+                        var existingMovieToUpdate = context.Movies.FirstOrDefault(movie => movie.Title == titleOfMovieToUpdate);
+
+                        if (existingMovieToUpdate != null)
+                        {
+                            Console.Write("Enter a new name for the movie: ");
+                            existingMovieToUpdate.Title = Console.ReadLine();
+
+                            context.SaveChanges();
+                        }
+                        else
+                        {
+                            Console.WriteLine($"No movie with title {titleOfMovieToUpdate} to update");
+                        }
+                        break;
+
+                    case "D":
+                        Console.Write("What is the name of the movie you want to delete: ");
+                        var titleOfMovieToDelete = Console.ReadLine();
+
+                        var existingMovieToDelete = context.Movies.FirstOrDefault(movie => movie.Title == titleOfMovieToDelete);
+
+                        if (existingMovieToDelete != null)
+                        {
+                            context.Movies.Remove(existingMovieToDelete);
+                            context.SaveChanges();
+                        }
+                        else
+                        {
+                            Console.WriteLine($"No movie with title {titleOfMovieToDelete} to update");
+                        }
+                        break;
                 }
             }
-
-
         }
     }
 }
